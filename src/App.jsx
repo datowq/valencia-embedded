c11eb7580a8170aa4e8c930b6d23ab51f9991ec2
Andy <steven.a.lew@gmail.com>
2023-10-08 16:22:26 -0700

    fix: use HashRouter because I forgot about client-side rendering with ghpages :')

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import Emote from './Emote'
 import Resume from './Resume'
 
 const App = () => {
-
-  const base = '/valencia-embeded'
-
   return (
     <Router>
       <Routes>
-        <Route path={`${base}/`} element={<Emote />} />
-        <Route path={`${base}/resume`}  element={<Resume />} />
-        <Route path={`${base}/about`}  element={<Resume />} />
-        <Route path={`${base}/contact`}  element={<Resume />} />
-        <Route path={`${base}/projects`}  element={<Resume />} />
-        <Route path={`${base}/credits`}  element={<Resume />} />
+        <Route path='/resume' element={<Resume />} />
+        <Route path='/about'  element={<Resume />} />
+        <Route path='/contact' element={<Resume />} />
+        <Route path='/projects' element={<Resume />} />
+        <Route path='/credits' element={<Resume />} />
+        <Route path='/' exact element={<Emote />} />
+        <Route path='*' element={<Emote />} />
       </Routes>
     </Router>
   )
